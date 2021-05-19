829bb6643601fc5674b8b4f19da50632019fc781
pc <pc@qq.com>
2021-05-19 16:32:48 +0800

    背包缩放，DOTween实现；

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,10 +1,17 @@
 
+using System.Numerics;
+using DG.Tweening;
 using UnityEngine;
 public class Main : MonoBehaviour
 {
     void Awake()
     {
         MonoSingleton.Instance.MonoGo.AddComponent<LuaClient>();
+
+        // float a = 10;
+        // DOTween.To(() => a, (x) => a = x, 10, 100).SetEase(Ease.Linear) ;t
+        // transform.DOLocalRotate()
+        // Vector3
     }
 
 }
